d735c8b2ab868ce352a67c41a13bbe49de2f86bf
toothtaste <197402001+toothtaste@users.noreply.github.com>
2025-09-24 01:32:21 +0700

    feat(reown): config

--- app/lib/providers/index.tsx
+++ app/lib/providers/index.tsx
@@ -4,16 +4,32 @@ import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-c
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { http } from "viem"
 import { base } from "viem/chains"
-import { createConfig, WagmiProvider } from "wagmi"
+import { cookieStorage, createConfig, createStorage, WagmiProvider } from "wagmi"
+import { mainnet, arbitrum } from "@reown/appkit/networks"
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi"
 
-const wagmiConfig = createConfig({
-  chains: [base],
-  transports: {
-    [base.id]: http(),
-  },
-  // @ts-ignore
-  connectors: [miniAppConnector()],
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+if (!projectId) throw new Error("Project ID is not defined")
+
+const networks = [mainnet, arbitrum]
+
+const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+  projectId,
+  networks,
 })
+
+// const wagmiConfig = createConfig({
+//   chains: [base],
+//   transports: {
+//     [base.id]: http(),
+//   },
+//   // @ts-ignore
+//   connectors: [miniAppConnector()],
+// })
 
 const tanstackQueryClient = new QueryClient({
   defaultOptions: {
@@ -27,7 +43,7 @@ const tanstackQueryClient = new QueryClient({
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={wagmiConfig}>
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
       <QueryClientProvider client={tanstackQueryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   )
