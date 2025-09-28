52e4a82a743df98287ed841c072aa307ada7b5ad
marbleheartg <197402001+marbleheartg@users.noreply.github.com>
2025-09-29 01:48:02 +0700

    feat: wagmi-cli

---
+++ wagmi.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "@wagmi/cli";
+import { Abi } from "viem";
+import reviewsAbi from "./artifacts/contracts/Reviews.sol/Reviews.json";
+
+export default defineConfig({
+  out: "artifacts/contracts/Reviews.sol/generated.ts",
+  contracts: [
+    {
+      name: "Reviews",
+      abi: reviewsAbi.abi as Abi,
+    },
+  ],
+  plugins: [],
+});
