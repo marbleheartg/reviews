ae8c4e03fd075f76642ca5d90c11f143d3cae91c
marbleheartg <197402001+marbleheartg@users.noreply.github.com>
2025-09-29 02:14:25 +0700

    chore

--- ignition/modules/Reviews.ts
+++ ignition/modules/Reviews.ts
@@ -2,16 +2,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("ReviewsModule", (m) => {
   const Contract = m.contract("Reviews", [
-    process.env.TESTING_WALLET_ADDRESS as `0x${string}`,
+    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
   ]);
-
-  // const testingWallet = process.env.TESTING_WALLET_ADDRESS;
-
-  // const from = m.getAccount(0);
-
-  // m.send("fundTestingWallet", testingWallet!, parseEther("100"), undefined, {
-  //   from,
-  // });
 
   return { Contract };
 });
